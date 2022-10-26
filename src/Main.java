5273800c7f4028bc4bebe917d4f40aba0f9b99e9
Rumonz <lone_assassin@mail.ru>
2022-10-27 01:11:02 +0300

    выполнил задание 2 из домашнего задания 3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        //Задание 1
         int age =42;
         boolean kinderGarden = age>=2 && age <=6;
         boolean school = age >=7&&age<=18;
@@ -14,5 +15,19 @@ public class Main {
         } else if (kinderGarden) {
             System.out.println("Если возраст человека равен "+age+", то ему нужно ходить в детский сад!");
         }
+        //Задание 2
+        int humanAge = 12;
+        boolean canNotUseAttraction = humanAge < 5 && humanAge>=0;
+        boolean canUseAttractionButWithAdult = humanAge >=5 && humanAge<14;
+        boolean canUseAttraction = humanAge >= 14;
+        if (canUseAttraction){
+            System.out.println("Ребёнок может кататься на аттракционе без сопровождения взрослого.");
+        } else if (canUseAttractionButWithAdult) {
+            System.out.println("Ребёнок может кататься только в сопровождении взрослого. Если взрослого нет, то кататься нельзя.");
+        } else if (canNotUseAttraction) {
+            System.out.println("Ребёнок не может кататься на аттракционе!");
+        }
+        //Задание 3
+
     }
 }
