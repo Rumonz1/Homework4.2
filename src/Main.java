0f59f7a79037869260cfae65c0aabbe20cef6b06
Rumonz <lone_assassin@mail.ru>
2022-10-27 01:23:37 +0300

    выполнил задание 3 из домашнего задания 3

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         //Задание 1
-        int age =42;
+        int age =3;
         boolean kinderGarden = age>=2 && age <=6;
         boolean school = age >=7&&age<=18;
         boolean university = age>18&age<24;
@@ -12,7 +12,7 @@ public class Main {
             System.out.println("Если возраст человека равен "+age+", то ему нужно ходить в университет!");
         } else if (school) {
             System.out.println("Если возраст человека равен "+age+", то ему нужно ходить в школу!");
-        } else if (kinderGarden) {
+        } else{
             System.out.println("Если возраст человека равен "+age+", то ему нужно ходить в детский сад!");
         }
         //Задание 2
@@ -24,10 +24,22 @@ public class Main {
             System.out.println("Ребёнок может кататься на аттракционе без сопровождения взрослого.");
         } else if (canUseAttractionButWithAdult) {
             System.out.println("Ребёнок может кататься только в сопровождении взрослого. Если взрослого нет, то кататься нельзя.");
-        } else if (canNotUseAttraction) {
+        } else{
             System.out.println("Ребёнок не может кататься на аттракционе!");
         }
         //Задание 3
-
+        int one=1;
+        int two=2;
+        int three=3;
+        boolean oneBigger = three < one && two < one;
+        boolean twoBigger = three <two && three < two;
+        boolean threeBigger = two<three&&one<three;
+        if (threeBigger) {
+            System.out.println("Число " + three + ", бОльшее!");
+        } else if (twoBigger) {
+            System.out.println("Число "+two+", бОльшее");
+        }else{
+            System.out.println("Число "+one+", бОльшее!");
+        }
     }
 }
